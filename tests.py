967650fc2694138d5c2f87c2bb7af588f5e06d6d
git <tamarsh224@gmail.com>
2023-08-19 19:53:30 +0300

    changed pytest commands

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 import requests
 
 
